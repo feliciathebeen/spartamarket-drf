d62b1a46da6288451e482c6987ab0fe1082d6224
feliciathebeen <feliciathebeen@gmail.com>
2024-09-04 23:03:21 +0900

    product list, detail

--- products/urls.py
+++ products/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from . import views
 
+app_name = "products"
 urlpatterns = [
-
+    path("", views.product_list, name="article_list"),
+    path("<int:pk>/", views.product_detail, name="product_detail"),
 ]
--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .serializers import ArticleSerializer
+from .models import Product
 
-# Create your views here.
+
+@api_view(["GET"])
+def product_list(request):
+    products = Product.objects.all()
+    serializer = ArticleSerializer(products, many=True)
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    serializer = ArticleSerializer(product)
+    return Response(serializer.data)
