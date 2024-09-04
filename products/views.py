07d12893242a938b630846983d507b0626b6348c
feliciathebeen <feliciathebeen@gmail.com>
2024-09-04 23:57:09 +0900

    product create, put, delete

--- products/views.py
+++ products/views.py
@@ -3,17 +3,36 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ArticleSerializer
 from .models import Product
+from rest_framework import status
 
 
-@api_view(["GET"])
+@api_view(["GET", "POST"])
 def product_list(request):
-    products = Product.objects.all()
-    serializer = ArticleSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        products = Product.objects.all()
+        serializer = ArticleSerializer(products, many=True)
+        return Response(serializer.data)
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(["GET"])
+@api_view(["GET", "DELETE", "PUT"])
 def product_detail(request, pk):
     product = get_object_or_404(Product, pk=pk)
-    serializer = ArticleSerializer(product)
-    return Response(serializer.data)
+    if request.method == "GET":
+        serializer = ArticleSerializer(product)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        serializer = ArticleSerializer(product, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+    elif request.method == "DELETE":
+        product.delete()
+        data = {"delete": f"Product({pk}) is deleted."}
+        return Response(data, status=status.HTTP_200_OK)
